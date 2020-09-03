8c10edbf2e788c7056407fd1370cc273c752225c
Arie De Bok <arie@directdebit.co.za>
2020-09-03 09:21:06 +0200

    Convert total_seconds to integers

--- heartbeat_sh/heartbeat.py
+++ heartbeat_sh/heartbeat.py
@@ -14,9 +14,9 @@ class HeartbeatClient:
     def send_beat(self, name: str, warning_timeout: timedelta = None, error_timeout: timedelta = None):
         query = ""
         if warning_timeout is not None:
-            query += f"?warning={warning_timeout.total_seconds()}"
+            query += f"?warning={int(warning_timeout.total_seconds())}"
         if error_timeout is not None:
             query += "&" if len(query) > 0 else "?"
-            query += f"error={error_timeout.total_seconds()}"
+            query += f"error={int(error_timeout.total_seconds())}"
 
         requests.post(f"{self.base_url}beat/{name}{query}")
