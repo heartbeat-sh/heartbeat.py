9760b1e020e530914fe6e959a06dbed9814e99ff
Arie De Bok <arie@directdebit.co.za>
2022-03-22 09:10:01 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="heartbeat-sh",
-    version="0.1.0",
+    version="0.2.0",
     author="heartbeat.sh",
     author_email="admin@heartbeat.sh",
     description="A client library for heartbeat.sh",
