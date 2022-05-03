89b34cd1a8e9d6bebd32611a5e09acb5b1961fd4
dolf <rudolfbyker@gmail.com>
2022-05-03 08:47:55 +0200

    #4 Dependency Injection.
    Use Dependency Injection to allow the user to use any request back end they like by injecting a request function.
    Fall back to using the `requests` library (with a local import so that we don't have to depend on `requests`).
    Default request timeout is now 3 seconds.
    Updated README.md
    Added unit tests using DI.

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 This is a Python client library for [heartbeat.sh](https://heartbeat.sh).
 
-## Usage
+## Quick start
 
-Install with `pip install heartbeat-sh`
+Install with `pip install heartbeat-sh requests`
 
-```Python
+```python
 from datetime import timedelta
 from heartbeat_sh import HeartbeatClient
 
 HeartbeatClient("example").send_beat(
+    "example:python",
+    timedelta(days=1, hours=2),
+    timedelta(days=2)
+)
+```
+
+## Use a custom requests library (Dependency Injection)
+
+By default, this module uses the [`requests`](https://docs.python-requests.org/en/latest/) library to make HTTP 
+requests. This is not a requirement. You may inject any request library:
+
+```python
+from datetime import timedelta
+from heartbeat_sh import HeartbeatClient
+
+def request(url: str, method: str):
+    return {
+        "fake": "json result"
+    }
+
+HeartbeatClient(
+    subdomain="example",
+    request=request,
+).send_beat(
     "example:python",
     timedelta(days=1, hours=2),
     timedelta(days=2)
--- heartbeat_sh/heartbeat.py
+++ heartbeat_sh/heartbeat.py
@@ -1,21 +1,31 @@
 from datetime import timedelta
+from typing import Any, Protocol
 
-import requests
+
+class RequestCallable(Protocol):
+    def __call__(self, *, method: str, url: str) -> Any:
+        ...
 
 
 class HeartbeatClient:
-    def __init__(self, subdomain: str):
+    def __init__(self, subdomain: str, request: RequestCallable = None):
         self.subdomain = subdomain
+        self.request = request or _default_request
 
         self.proto = "https"
         self.host = "heartbeat.sh"
         self.base_url = f"{self.proto}://{self.subdomain}.{self.host}/"
 
     def get_beats(self):
-        r = requests.get(f"{self.base_url}heartbeats")
-        return r.json()["Heartbeats"]
+        r = self.request(method="GET", url=f"{self.base_url}heartbeats")
+        return r["Heartbeats"]
 
-    def send_beat(self, name: str, warning_timeout: timedelta = None, error_timeout: timedelta = None):
+    def send_beat(
+        self,
+        name: str,
+        warning_timeout: timedelta = None,
+        error_timeout: timedelta = None,
+    ):
         query = ""
         if warning_timeout is not None:
             query += f"?warning={int(warning_timeout.total_seconds())}"
@@ -23,7 +33,18 @@ class HeartbeatClient:
             query += "&" if len(query) > 0 else "?"
             query += f"error={int(error_timeout.total_seconds())}"
 
-        return requests.post(f"{self.base_url}beat/{name}{query}")
+        return self.request(method="POST", url=f"{self.base_url}beat/{name}{query}")
 
     def delete_beat(self, name):
-        return requests.delete(f"{self.base_url}beat/{name}")
+        return self.request(method="DELETE", url=f"{self.base_url}beat/{name}")
+
+
+def _default_request(*, method: str, url: str) -> Any:
+    # noinspection PyUnresolvedReferences
+    import requests  # Local import, because this is an optional dependency.
+
+    return requests.request(
+        method=method,
+        url=url,
+        timeout=3,
+    ).json()
